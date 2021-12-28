c6c80a917886a511071a5a62659c333afe6e6cbd
Taylor Abraham <taylor.abra8@gmail.com>
2021-12-27 21:53:21 -0500

    feat(home): splash screen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Home.scss';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home__splash-img">
+      <div className="home__hero-text">
+        <h1>YUGIOH REBORN</h1>
+        <h3>A format with the best of 2014 Yugioh with fixes, tweaks, and more!</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
