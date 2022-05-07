b9a5ac9e2abbebf14b65f03187df9c265651e271
Taylor Abraham <taylor.abra8@gmail.com>
2022-05-07 14:32:14 -0400

    chore(home): Minor stylings

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import './Home.scss';
 
 const Home = () => {
   return (
-    <div className="content home__splash-img">
-      <div className="home__hero-text">
-        <h1>YUGIOH REBORN</h1>
-        <h3>A format with the best of 2014 Yugioh with fixes, tweaks, and more!</h3>
-      </div>
-    </div>
+    <Box className="home content">
+      <Box className="home__splash-img">
+        <Box className="home__hero-text">
+          <Typography mb={2} variant="h2">
+            YUGIOH REBORN
+          </Typography>
+          <Typography variant="body1">
+            A format with the best of 2014 Yugioh with fixes, tweaks, and more!
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
