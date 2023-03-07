897e4ae6297cb592cd745bc9c7d9d3f8df5d423d
Taylor Abraham <taylor.abra8@gmail.com>
2023-03-07 17:12:12 -0500

    chore: change server URL to Render

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const SERVER_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : 'https://yugioh-reborn-server.herokuapp.com';
+    : 'https://yugioh-reborn-server.onrender.com';
 
 export enum SERVER_ERRORS {
   SERVER_NOT_STARTED = 'SERVER_NOT_STARTED',
